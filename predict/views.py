99f5f7397ac712fd7b0be95d97de80e5647ffd3d
Musadiq-Aliyev <43294124+Musadiq-Aliyev@users.noreply.github.com>
2020-08-02 09:09:02 +0400

    View updated

--- predict/views.py
+++ predict/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def predict(request):
     return render(request,'prediction.html')
+
+def predict_chances(request):
+
+    if request.POST.get('action')=='post':
+
+        sepal_length=float(request.POST.get('sepal_length'))
+        sepal_width=float(request.POST.get('sepal_width'))
+        petal_length=float(request.POST.get('petal_length'))
+        petal_width=float(request.POST.get('petal_width'))
